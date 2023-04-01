909a32111584bf06decb96b83b9e262e1434ee2e
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-01 15:52:27 +0200

    styling

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -32,7 +32,7 @@ const dataFormating = (dateToFormat: any) => {
 }
 
 return (
-    <div className="flex flex-col-reverse overflow-auto items-center gap-2 p-2 h-[92vh] w-screen">
+    <div className="flex flex-col-reverse overflow-auto items-center gap-2 p-2 pb-5 h-[80vh] w-screen bg-slate-400 border-b-orange-400 border-2">
       {chat.map(el => (
         <div key={el.id} className="rounded-xl bg-blue-600 px-1 py-1 max-w-[40%] text-center shadow flex flex-row justify-end flex-wrap m-1">
           <h1 className="w-96 text-xs text-blue-300 ">{el.title}</h1>
